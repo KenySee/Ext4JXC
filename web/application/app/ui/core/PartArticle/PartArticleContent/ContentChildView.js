54538932559e52fc4620879dcfbcc7a1324dd0c5
zhoubo <zb@zallhy.com>
2017-11-16 15:54:45 +0800

    文章管理

---
+++ web/application/app/ui/core/PartArticle/PartArticleContent/ContentChildView.js
@@ -0,0 +1,107 @@
+Ext.define('Keer.ui.core.PartArticle.PartArticleContent.ContentChildView', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.ui-core-PartArticle-PartArticleContent-ContentChildView',
+    requires: [
+        'Keer.widget.field.FileTrigger',
+        'Keer.ui.core.PartArticle.PartArticleContent.tpl.JustifyTextView',
+        'Keer.ui.core.PartArticle.PartArticleContent.tpl.BigImageView',
+        'Keer.ui.core.PartArticle.PartArticleContent.tpl.ImageProductView',
+        'Keer.ui.core.PartArticle.PartArticleContent.tpl.CenterTextView'
+    ],
+    frame: false,
+    border: 0,
+    layout: 'border',
+    bodyBorder: false,
+    defaults: {
+        collapsible: false,
+        split: true
+    },
+    bodyPadding: 5,
+    contentType:"",
+    loadData: function (contentType, data) {
+        //根据类型加载数据
+        var dataIndex = 0;
+        var grid = this.queryById('westItem');
+        dataIndex=grid.getStore().find('type',contentType)
+        if(dataIndex!=-1){
+            grid.getSelectionModel().select(dataIndex, true, false);
+            var formItem = this.queryById('formItem');
+            formItem.loadData(data);
+            grid.setDisabled(true);
+        }
+        // this.getForm().setValues(data);
+    },
+    fetchData: function () {
+        var formItem = this.queryById('formItem');
+        if(formItem){
+            return {contentType:this.contentType,data:formItem.fetchData()};
+        }else{
+            Ext.Msg.alert('提示','请选择类型');
+            return;
+        }
+        // return this.getForm().getValues();
+    },
+    clearData: function () {
+        var cmp = this.queryById('centerItem');
+        cmp.removeAll(true);
+        cmp.add(Ext.create("Keer.ui.core.PartArticle.PartArticleContent.tpl." + this.contentType+"View", {itemId:'formItem'}));
+    },
+    items: [
+        {
+            width: 250,
+            listeners: {
+                selectionchange: function (me, selected, eOpts) {
+                    var cmp = this.up('panel').queryById('centerItem');
+                    cmp.removeAll(true);
+                    this.up('panel').contentType = selected[0].get("type");
+                    cmp.add(Ext.create("Keer.ui.core.PartArticle.PartArticleContent.tpl." + selected[0].get("type")+"View", {itemId:'formItem'}));
+                }
+            },
+            itemId: 'westItem',
+            frame: false,
+            region: 'west',
+            xtype: 'grid',
+            columnLines: true,
+            hideHeaders: true,
+            viewConfig: {
+                forceFit: true,
+                stripeRows: true//在表格中显示斑马线
+            },
+            store: {
+                fields: [
+                    {name: 'name', type: 'string'},
+                    {name: 'type', type: 'string'},
+                    {name: 'image', type: 'string'}
+                ],
+                xtype: 'store.array',
+                data: [{name: '段落大图', type: 'BigImage', image: '/images/EFDFEEE9-6ABA-450E-A3DD-66492D648466.png'},
+                    {name: '段落文本', type: 'JustifyText', image: '/images/01511AA0-3AD4-455D-B7FF-22A2DAFDDF79.png'},
+                    {name: '居中文本', type: 'CenterText', image: '/images/291A9F24-87E5-45AD-B2DA-B3017F39F28F.png'},
+                    {name: '作品预订', type: 'ImageProduct', image: '/images/846642E5-BA36-42B0-BD4E-124F4DD43885.png'}
+                ],//读取内嵌数据
+                autoLoad: true//自动加载
+            },
+            columns: [
+                {
+                    text: '添加类型',
+                    dataIndex: 'image',
+                    width: '100%',
+                    renderer: function (value, metaData, record, rowIndex, colIndex, store, view) {
+                        if (value)
+                            return "<a href='javascript:void(0);' target='_blank' title='" + record.get('name') + "'><img width='100%' height='100' src='" + value + "'/></a>";
+                        else
+                            return value;
+                    }
+
+                }
+            ]
+        }, {
+            region: 'center',
+            xtype: 'panel',
+            frame: false,
+            layout: 'fit',
+            itemId: 'centerItem',
+            html: '点击左侧图片选择添加的类型'
+        }
+    ]
+});
--- web/application/app/ui/core/PartArticle/PartArticleContent/tpl/JustifyTextView.js
+++ web/application/app/ui/core/PartArticle/PartArticleContent/tpl/JustifyTextView.js
@@ -1,116 +1,35 @@
-Ext.create('Ext.data.Store', {
-    storeId:'justifyTextStore',
-    fields:['text','id','bFont'],
-    data:{
-        items:[]
-    },
-    proxy: {
-        type: 'memory',
-        reader: {
-            type: 'json',
-            root: 'items'
-        }
-    }
-});
 Ext.define('Keer.ui.core.PartArticle.PartArticleContent.tpl.JustifyTextView',{
     extend: 'Ext.form.Panel',
     alias: 'widget.ui-core-PartArticle-tpl-JustifyTextView',
     requires: [
+        'Keer.widget.field.FileTrigger'
     ],
     frame:false,
     border:0,
     bodyPadding: 5,
     loadData:function (data) {
         this.getForm().setValues(data);
-        var grid = this.down('grid');
-        var store = grid.getStore();
-        store.loadData(data.sectionContent||[]);
     },
     fetchData:function () {
-        var data = this.getForm().getValues();
-        var grid = this.down('grid');
-        var store = grid.getStore();
-        var sectionContent = [];
-        store.each(function (model) {
-            sectionContent.push(model.getData());
-        });
-        data['sectionContent'] = sectionContent;
-        return data;
+        return this.getForm().getValues();
     },
     items:{
         layout: 'column',
         border:0,
         items: [{
-            columnWidth: 1/3,
-            layout: 'form',
-            border:0,
-            margin: '2 10 2 10',
-            items: [
-                {
-                    fieldLabel: '段落序号',
-                    name: 'sectionIndex',
-                    itemId: 'sectionIndex',
-                    dataIndex: 'sectionIndex',
-                    xtype:'numberfield'
-                }
-            ]
-        },{
-            columnWidth: 1/2,
-            layout: 'form',
-            border:0,
-            margin: '2 10 2 10',
-            items: [
-                {
-                    fieldLabel: '段落标题',
-                    name: 'sectionText',
-                    itemId: 'sectionText',
-                    dataIndex: 'sectionText',
-                    xtype:'textfield'
-                }
-            ]
-        },{
             columnWidth: 1,
-            layout: 'fit',
-            xtype:'grid',
+            layout: 'form',
             border:0,
             margin: '2 10 2 10',
-            plugins:[
-                Ext.create('Ext.grid.plugin.CellEditing', {
-                    pluginId: 'cellplugin',
-                    clicksToEdit: 1
-                })
-            ],
-            listeners:{
-                'selectionchange':function ( opt, selected) {
-                    var button = opt.view.up('window').down('[iconCls=remove]')
-                    button.setDisabled(false);
-                    button.record = selected;
+            items: [
+                {
+                    fieldLabel: '居中文本',
+                    name: 'textContent',
+                    itemId: 'textContent',
+                    dataIndex: 'textContent',
+                    xtype:'textarea',
+                    rows:10
                 }
-            },
-            dockedItems:{
-                xtype: 'toolbar',
-                dock: 'top',
-                items: [
-                    {text:'新增',iconCls:'add',iconAlign: 'left',handler:function(){
-                        var grid = this.up('grid');
-                        var store = grid.getStore();
-                        var model = store.createModel({id:store.getCount()+1});
-                        store.add(model);
-                        var editor = grid.getPlugin('cellplugin');
-                        editor.startEditByPosition({row: store.getCount()-1, column: 1});
-                    }},
-                    {text:'删除',iconCls:'remove',iconAlign: 'left',disabled:true,handler:function(){
-                        var grid = this.up('grid');
-                        var store = grid.getStore();
-                        store.remove(this.record);
-                    }}
-                ]
-            },
-            store: Ext.data.StoreManager.lookup('justifyTextStore'),
-            columns: [
-                { text: '序号',  dataIndex: 'id', width:80 },
-                { text: '粗体',  dataIndex: 'bFont',xtype : 'checkcolumn', width:80 },
-                { text: '文本',  dataIndex: 'text', flex: 1,editor:{xtype:'textfield'} }
             ]
         }]
     }
