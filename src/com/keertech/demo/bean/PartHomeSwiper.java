f9d1581117f672dd8adda14a3d2aa461c00c7e42
samil1980 <samil1980@126.com>
2017-11-20 10:44:06 +0800

    提交 轮播图模块代码

--- src/com/keertech/demo/bean/PartHomeSwiper.java
+++ src/com/keertech/demo/bean/PartHomeSwiper.java
@@ -1,6 +1,7 @@
 package com.keertech.demo.bean;
 
 import com.keer.core.annotation.Description;
+import com.keer.core.bean.base.BaseBean;
 import com.keer.core.bean.base.GenericBean;
 
 import javax.persistence.Entity;
@@ -9,13 +10,24 @@ import javax.persistence.Table;
 @Entity
 @Table(name="part_home_swiper")
 @Description(Name="首页轮播")
-public class PartHomeSwiper extends GenericBean {
+public class PartHomeSwiper extends BaseBean {
 
     @Description(Name="图片地址")
     private String coverImg;
 
     @Description(Name="链接地址")
     private String pathUrl;
+
+    @Description(Name="轮播标题")
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     public String getCoverImg() {
         return coverImg;
